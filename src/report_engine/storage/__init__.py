501156c70c9c59347be9448c41d08ec872e457e4
Juntao Chi <476409177@qq.com>
2026-07-17 06:53:09 +0800

    feat: publish report catalog atomically

--- src/report_engine/storage/__init__.py
+++ src/report_engine/storage/__init__.py
@@ -1,5 +1,6 @@
 """Atomic publication of completed report bundles."""
 
 from report_engine.storage.bundle import BundlePublisher
+from report_engine.storage.catalog import CatalogPublicationError, CatalogPublisher
 
-__all__ = ["BundlePublisher"]
+__all__ = ["BundlePublisher", "CatalogPublicationError", "CatalogPublisher"]
